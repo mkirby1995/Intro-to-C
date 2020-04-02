fc717adc4e0dffdf246474604f0cce723e0cd50d
Matt.Kirby <mkirby3@angelo.edu>
2020-04-02 10:23:16 -0500

    update

--- queue/queue.c
+++ queue/queue.c
@@ -11,39 +11,69 @@ typedef struct Queue {
 /*
     Creates a queue by allocating the appropriate amount of memory for an
     instance of the Queue struct, and initializes all of the fields of the
-    struct. Also allocates memory for the queue's storage structure. 
+    struct. Also allocates memory for the queue's storage structure.
 */
 Queue *createQueue(unsigned capacity)
 {
-
-}
+    Queue *newQueue = malloc(sizeof(Queue));
+    newQueue->storage = malloc(capacity * sizeof(unsigned int));
+    newQueue->length = 0;
+    newQueue->capacity = capacity;
+    return newQueue;
+};
 
 /*
     Adds the given item to the end of the queue. If the queue does
-    not have room, expand the queue's available storage so that it 
+    not have room, expand the queue's available storage so that it
     does have room for the additional item.
 */
+
 void enqueue(Queue *q, int item)
 {
-
-}
+    if (q->length == q->capacity) {
+        printf("Resizing from %d to %d\n", q->capacity, q->capacity * 2);
+    };
+    *(q->storage + q->length) = item;
+    printf("Adding %d to the Queue\n", *(q->storage + q->length));
+    unsigned int k = 0;
+    for (k = 0; k <= q->length; k++) {
+        printf("Value %d: %d\n", k, *(q->storage + k));
+    };
+    q->length ++;
+};
 
 /*
-    Removes the item at the front of the queue and returns it. 
+    Removes the item at the front of the queue and returns it.
     If the queue is empty, this function should return -1.
 */
 int dequeue(Queue *q)
 {
-
-}
+    unsigned int k = 0;
+    for (k = 0; k <= q->length; k++) {
+        printf("Value %d: %d\n", k, *(q->storage + k));
+    };
+    printf("Queue length: %d\n", q->length);
+    if (q->length != 0) {
+        unsigned int i = 0;
+        int return_value = *q->storage;
+        for (i = 0; i <= q->length; i++) {
+            *(q->storage + i) = *(q->storage + i + 1);
+        }
+        q->length --;
+        return return_value;
+      } else {
+        return 0;
+      }
+};
 
 /*
     Frees the memory used to hold the queue instance and its
-    associated storage. 
+    associated storage.
 */
 void destroyQueue(Queue *q)
 {
-
+    free(q->storage);
+    free(q);
 }
 
 
